5d45db6e8444c869493fd35a0d34d8ce6ba0c7dd
PrakashML <prakashkrishnan08@gmail.com>
2024-01-19 09:38:07 +0530

    Feature Added: EmpWage_uc3

--- WageComputation/src/com/WageComputation/WageConputation.java
+++ WageComputation/src/com/WageComputation/WageConputation.java
@@ -1,17 +1,23 @@
 package com.WageComputation;
 
 public class WageConputation {
+    public static final int Part_t = 1;
+    public static final int Full_t = 2;
+    public static final int wph = 20;
+
     public static void main(String[] args) {
-        int present = 1;
-        int wph = 20;
         int empHrs = 0;
         int empWage = 0;
         double empCheck = Math.floor(Math.random()*10)%2;
-        if(empCheck == present){
-            empHrs = 8;
-        }
-        else{
-            empHrs = 0;
+        switch ((int) empCheck){
+            case Part_t:
+                empHrs = 4;
+                break;
+            case Full_t:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
         }
         empWage = empHrs * wph;
         System.out.println("Employee Wage: " + empWage);
