448e7870d955dc4b10967db2a0f63ed9aa9c7da2
PrakashML <prakashkrishnan08@gmail.com>
2024-01-19 07:08:26 +0530

    Feature Added: EmpWage_uc2

--- WageComputation/src/com/WageComputation/WageConputation.java
+++ WageComputation/src/com/WageComputation/WageConputation.java
@@ -3,12 +3,17 @@ package com.WageComputation;
 public class WageConputation {
     public static void main(String[] args) {
         int present = 1;
+        int wph = 20;
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random()*10)%2;
         if(empCheck == present){
-            System.out.println("Employee is present");
+            empHrs = 8;
         }
         else{
-            System.out.println("Employee is absent");
+            empHrs = 0;
         }
+        empWage = empHrs * wph;
+        System.out.println("Employee Wage: " + empWage);
     }
 }
